336c0d2ddd7b23afbc66f8be40697c99da8b1b59
Nautilus <67490989+longtoZ@users.noreply.github.com>
2022-09-01 18:27:48 +0700

    Update parselinks.py

--- Code/parselinks.py
+++ Code/parselinks.py
@@ -2,20 +2,13 @@ import json
 import bs4
 import subprocess
 import sys
+import re
 
 def checker(package):
     #return str(subprocess.run(["pip", "freeze"], capture_output=True, text=True).stdout).split('\n')
     return str(subprocess.run(["pip", "install", package], capture_output=True, text=True).stdout)
 
-def sorting_function(temp):
-	for i in range(len(temp)):
-		if temp[i:i+5] == '-bai-':
-			key = i+5
-			break
-	ordinal = int(temp[key:key+2].replace('-',''))
-	return ordinal
-
-def parser(filename, type):
+def parser(filename, type, chapter=None):
 	links = []
 
 	with open(filename, encoding='utf-8') as f:
@@ -31,19 +24,30 @@ def parser(filename, type):
 			pass
 
 	links = [f"{i.replace('__sd', '')[:i.find('?nimblesessionid')-4]}" for i in links]
+	links.sort(key=lambda n: int(n[n.find('-bai-')+5 : n.find('-', n.find('-bai-')+5 )]))
 
-# shorting part
-	links.sort(key=sorting_function)
+	full = []
+	for link in links:
+		raw_title = re.findall("-bai-\d+-[0-9a-zA-z\-]+\.mp4", link)
+		title = raw_title[0][raw_title[0].find('bai-'):raw_title[0].find('.mp4')]
+
+		full.append([title, link])
 
 	if (type==1):
 		return links
 	elif (type==2):
-		return '\n'.join(links)
+		return '\n\n'.join(links)
+	elif (type==3):
+		txt = '#EXTM3U\n'
+		for i in full:
+			txt += f'#EXTINF:-1 group-title="{chapter}",{i[0]}\n{i[1]}\n\n'
+		return txt
 
-print(parser('test 6.har', 2))
+print(parser('omg.har', 3, 'Video Sửa Chữa Bo Mạch Điều Hòa Inverter Chung'))
 
 '''
 Sử dụng hàm "parser":
 	"1" nếu in dạng list
 	"2" nếu in từng dòng
+	"3" để in full
 '''
